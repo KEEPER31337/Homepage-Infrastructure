8f1e3ae495e912c0b392786adc70e5e6f7b0ab1b
Jun Seol <physiogel@pusan.ac.kr>
2022-05-14 09:30:45 +0900

    fix: docker file context 수정

--- chat/build.sh
+++ chat/build.sh
@@ -1,2 +1,2 @@
-docker build -t keeper-homepage-chat:${CHAT_IMAGE_TAG} -f docker/chat.dockerfile .
-docker build -t keeper-homepage-chat-db:${CHAT_IMAGE_TAG} -f docker/db.dockerfile .
+docker build -t keeper-homepage-chat:${CHAT_IMAGE_TAG} -f docker/chat.dockerfile docker
+docker build -t keeper-homepage-chat-db:${CHAT_IMAGE_TAG} -f docker/db.dockerfile docker
