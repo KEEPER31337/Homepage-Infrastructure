6650aefaa880e0cbf7b86dc2eebf1043cfd63198
Jun Seol <physiogel@pusan.ac.kr>
2022-06-12 22:14:45 +0900

    fix: chat sh 수정

---
+++ chat/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker build -t keeper-homepage-chat:${CHAT_IMAGE_TAG} -f docker/chat.dockerfile docker
+docker build -t keeper-homepage-chat-db:${CHAT_IMAGE_TAG} -f docker/db.dockerfile docker
+
+docker-compose up -d
