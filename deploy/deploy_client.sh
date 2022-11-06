a3e8128a3e9d066f9b0e993a02ae98f2ac56f874
redundant4u <physiogel@pusan.ac.kr>
2022-11-06 21:48:30 +0900

    fix: 배포 스크립트 수정

--- deploy/deploy_client.sh
+++ deploy/deploy_client.sh
@@ -5,14 +5,14 @@ if [ $# -ne 1 ]; then
     exit -1
 fi
 
-BUILD_PATH="docker/data"
+BUILD_PATH="docker/data/client"
 
-aws s3 cp s3://keeper-homepage/prod/front/$1.tar.gz $BUILD_PATH
+aws s3 cp s3://keeper-homepage/prod/front/$1.tar.gz $BUILD_PATH/$1.tar.gz
 
-if [ -d $BUILD_PATH/client ]
+if [ -d $BUILD_PATH ]
 then
-    rm -rf $BUILD_PATH/client/*
+    rm -rf $BUILD_PATH/*
 fi
 
-tar -zxvf $BUILD_PATH/$1.tar.gz -C $BUILD_PATH/client --strip 1
+tar -zxvf $BUILD_PATH/$1.tar.gz -C $BUILD_PATH --strip 1
 rm $BUILD_PATH/$1.tar.gz
--- deploy/deploy_server.sh
+++ deploy/deploy_server.sh
@@ -5,11 +5,11 @@ if [ $# -ne 1 ]; then
     exit -1
 fi
 
-BUILD_PATH="docker/data"
+BUILD_PATH="docker/data/server"
 
-aws s3 cp s3://keeper-homepage/prod/back/$1.tar.gz $BUILD_PATH
+aws s3 cp s3://keeper-homepage/prod/back/$1.tar.gz $BUILD_PATH/$1.tar.gz
 
-if [ -d $BUILD_PATH/server/application ]
+if [ -d $BUILD_PATH/application ]
 then
     rm -r \
         $BUILD_PATH/dependencies \
@@ -18,9 +18,11 @@ then
         $BUILD_PATH/application
 fi
 
-tar -zxvf $BUILD_PATH/$1.tar.gz -C $BUILD_PATH/server --strip 1
+tar -zxvf $BUILD_PATH/$1.tar.gz -C $BUILD_PATH
 rm $BUILD_PATH/$1.tar.gz
 
 docker build -t keeper-homepage-app:prod -f docker/app.layer.dockerfile .
 
-docker-compose -f docker/docker-compose.yml up -d
+docker rmi $(docker images -f "dangling=true" -q)
+
+docker-compose -p keeper up -d
