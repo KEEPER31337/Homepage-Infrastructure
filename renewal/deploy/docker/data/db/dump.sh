ef25b12e615411c96d383988b864588b64c64eaf
SeongHyun Koo <koosh@SeongHyunui-MacBookPro.local>
2023-09-26 23:47:21 +0900

    feat: 정기적인 db 백업스크립트

--- renewal/deploy/docker/data/db/dump.sh
+++ renewal/deploy/docker/data/db/dump.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
-docker exec -it db-r2 mysqldump -u MYSQL_USERNAME -pMYSQL_PASSWORD keeper > keeper_dump_$(date +%Y%m%d).sql
-docker cp db-r2:keeper_dump_$(date +%Y%m%d).sql .
+CURRENT_DATE=$(date +%Y%m%d)
+PREVIOUS_WEEK_DATE=$(date -d "7 days ago" +%Y%m%d)
+BACKUP_FILENAME="${CURRENT_DATE}_dump.sql"
+PREVIOUS_WEEK_BACKUP_FILENAME="${PREVIOUS_WEEK_DATE}_dump.sql"
+
+docker exec db-r2 sh -c 'mysqldump -u root -p"$MYSQL_ROOT_PASSWORD" keeper' > ~/db-r2_backup/${BACKUP_FILENAME}
+
+if [ $? -eq 0 ]; then
+  echo "Database backup completed: ${BACKUP_FILENAME}"
+  if [ -e "${PREVIOUS_BACKUP_FILENAME}" ]; then
+    rm "${PREVIOUS_BACKUP_FILENAME}"
+    echo "Previous day's backup file deleted: ${PREVIOUS_BACKUP_FILENAME}"
+  fi
+else
+  echo "Database backup failed"
+fi
+
+#이 스크립트를 리눅스에다가 매일 05시마다 실행하도록 스케쥴링
+# crontab -e
